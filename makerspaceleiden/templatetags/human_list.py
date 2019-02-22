abd7fa0de6f22fc90772ba8e54c487e083f3b86f
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-02-22 12:53:29 +0100

    Simplified crud and listings.

---
+++ makerspaceleiden/templatetags/human_list.py
@@ -0,0 +1,16 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def human_list_with_commas(obj_list):
+    if not obj_list:
+        return ""
+
+    # also force to string.
+    l=len(obj_list)
+    if l==1:
+        return u"%s" % obj_list[0]
+
+    # Oxford comman or not ?!
+    return ", ".join(str(obj) for obj in obj_list[:l-1]) + " and " + str(obj_list[l-1])
+
---
+++ makerspaceleiden/views.py
@@ -0,0 +1,39 @@
+class MySimpleHistoryAdmin(SimpleHistoryAdmin):
+    object_history_template = "object_history.html"
+
+    # Bit risky - routes in to bypass for naughtyness in below showhistory.
+    #
+    def has_change_permission(self, request, obj):
+       return True
+
+@login_required
+def showhistory(request,aClass,pk,rev=None):
+    try:
+         o = aClass.objects.get(pk=pk)
+    except DoesNotExist:
+         return HttpResponse("History not found",  pk,status=404,content_type="text/plain")
+    context = {
+       'title': 'View history',
+    }
+    if rev:
+      revInfo = o.history.get(pk = rev)
+      historic = revInfo.instance
+      form = StorageForm(instance = historic)
+      context = {
+            'title': 'Historic record',
+            'label': revInfo,
+            'action': None,
+            'is_logged_in': request.user.is_authenticated,
+            'user' : request.user,
+            'form':  form,
+            'storage':  historic,
+            'history': True,
+            'item': o,
+            'back': f'{o.__class__.__name__}_overview'
+      }
+      return render(request, 'crud.html', context)
+      # return historyAdmin.history_form_view(request,str(pk),str(rev), context)
+
+    historyAdmin = MySimpleHistoryAdmin(Storage,AdminSite())
+    return historyAdmin.history_view(request,str(pk),context)
+
