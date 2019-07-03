92498087a8960bcc50fa2c1b8721f3e2188402fd
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-07-03 19:53:21 +0200

    Bypass for staff to bootstrap new rules

--- acl/models.py
+++ acl/models.py
@@ -166,9 +166,10 @@ class Entitlement(models.Model):
         issuer_permit = PermitType.objects.get(pk = self.permit.pk)
 
         if issuer_permit and not Entitlement.objects.filter(permit=issuer_permit,holder=self.issuer):
-            logger.critical(f"Entitlement.save(): holder {self.issuer} cannot issue {self.permit} to {self.holder} as the holder lacks {issuer_permit}")
-            raise EntitlementViolation("issuer of this entitelment lacks the entitlement to issue it.")
-
+            if not user or not user.is_staff:
+                logger.critical(f"Entitlement.save(): holder {self.issuer} cannot issue {self.permit} to {self.holder} as the holder lacks {issuer_permit}")
+                raise EntitlementViolation("issuer of this entitelment lacks the entitlement to issue it.")
+            logger.critical(f"Entitlement.save(): STAFFF bypass of rule 'holder {self.issuer} cannot issue {self.permit} to {self.holder} as the holder lacks {issuer_permit}'")
         if self.active == None:
             # See if we can fetch an older approval for same that may already have
             # been activated. And grandfather it in.
