b9d7894548e66eb7292fc26aea1ffd58dea84eca
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-08-07 12:30:16 +0200

    Fix invisible / expirted tag blocking reswipe

--- unknowntags/models.py
+++ unknowntags/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 
 from members.models import Tag,clean_tag_string
 from acl.models import Entitlement,PermitType
@@ -11,14 +12,14 @@ class Unknowntag(models.Model):
    last_used = models.DateTimeField(auto_now=True)
 
    def save(self, * args, ** kwargs):
-      r = super(Unknowntag,self).save(*args, **kwargs)
-
-      stale_tags = Unknowntag.objects.all().order_by('-last_used')[settings.UT_COUNT_CUTOFF:]
+      days = settings.UT_DAYS_CUTOFF
+      cutoff = datetime.date.today() - datetime.timedelta(days=days)
+      stale_tags =  Unknowntag.objects.all().filter(Q(last_used__lt = cutoff))
 
       for stale_tag in stale_tags:
           stale_tag.delete()
 
-      return r
+      return super(Unknowntag,self).save(*args, **kwargs)
 
    def __str__(self):
          return "{} swiped on {}".format(self.tag, self.last_used.strftime("%Y-%m-%d %H:%M:%S"))
