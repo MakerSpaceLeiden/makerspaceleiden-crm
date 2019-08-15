f92a9e8f84afc3fe39ef37dd98d5ea790fe936a0
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-08-15 21:36:01 +0200

    Fix accidental reuse of index name.

--- mainssensor/urls.py
+++ mainssensor/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('api/v1/mainssensor/resolve/<int:tag>', views.resolve, name='resolve'),
     path('api/v1/mainssensor/resolve', views.resolve, name='resolve'),
-    path('mainsensors', views.index, name='index'),
+    path('mainsensors', views.index, name='mainsindex'),
 ]
