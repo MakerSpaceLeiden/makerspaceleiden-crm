203b3c1bd26f8d463236d3064105c148cea4bcb9
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-08-15 21:20:02 +0200

    main sensor mapping and mngt.

---
+++ mainssensor/urls.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('api/v1/mainssensor/resolve/<int:tag>', views.resolve, name='resolve'),
+    path('api/v1/mainssensor/resolve', views.resolve, name='resolve'),
+    path('mainsensors', views.index, name='index'),
+]
---
+++ mainssensor/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.urls import path
+from django.http import HttpResponse
+from django.conf import settings
+from django.db.models import Q
+
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Mainssensor
+from members.models import Tag,User,clean_tag_string
+from acl.models import Entitlement,PermitType
+
+from makerspaceleiden.decorators import superuser_or_bearer_required
+
+import logging
+import datetime
+import re
+from django.utils import timezone
+
+logger = logging.getLogger(__name__)
+
+@csrf_exempt
+# Not required - simple innocent read at this time.
+# @superuser_or_bearer_required 
+def resolve(request, tag = None):
+  if request.POST:
+         return HttpResponse("XS denied\n",status=403,content_type="text/plain")
+
+  if tag:
+      try:
+           item = Mainssensor.objects.get(tag = tag);
+      except:
+           return HttpResponse("Unknown sensor id\n",status=404,content_type="text/plain")
+      items = [ item ]
+  else:
+      items = Mainssensor.objects.all().order_by('tag')
+
+  return render(request, 'sensors.txt', { 'items': items }, content_type="text/plain")
+
+@login_required
+def index(request):
+  items =  Mainssensor.objects.all().order_by('tag');
+
+  return render(request, 'sensors.html', { 
+               'has_permission': request.user.is_authenticated,
+               'items': items,
+		'user': request.user,
+         })
