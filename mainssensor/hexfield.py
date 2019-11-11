63a05cf74caf46239a4b38f72fa09f56fe2efba0
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-11-11 22:27:28 +0100

    Capture and make sure all hexes look the same.

--- mainssensor/hexfield.py
+++ mainssensor/hexfield.py
@@ -3,6 +3,16 @@ from django import forms
 
 import re
 
+def tohexstr(value):
+   if isinstance(value, str) or value is None:
+            strout = value
+   else:
+            strout = hex(value)[2:]
+            if (len(strout) < 4):
+              strout = (4 - len(strout)) * '0' + strout 
+
+   return strout.upper()
+
 class HexFormField(forms.CharField):
 
     default_error_messages = {
@@ -19,26 +29,29 @@ class HexFormField(forms.CharField):
             raise forms.ValidationError(self.error_messages['invalid_hex'])
 
         return value
+        return int(value,16)
 
-class HexField(models.IntegerField):
+    def compare(self, a, b):
+        return a is not b
+
+class HexField(models.Field):
     description = "Hexadecimal value"
+
+    def db_type(self, connection):
+        return 'integer UNSIGNED'
 
     def get_prep_value(self, value):
         return int(value, 16)
 
-    def to_python(self, value):
-        if isinstance(value, str) or value is None:
-            strout = value
-        else:
-            strout = hex(value)[2:]
-            if (len(strout) < 4):
-              strout = (4 - len(strout)) * '0' + strout 
+    def from_db_value(self, value, expr, conn):
+        return tohexstr(value)
 
-        return strout
+    def to_python(self, value):
+        return tohexstr(value)
 
     def formfield(self, **kwargs):
         kwargs['widget'] = forms.TextInput
-        return super().formfield(form_class = HexFormField, **kwargs)
+        # return super().formfield(form_class = HexFormField, **kwargs)
         kwargs['form_class'] = HexFormField
         kwargs['max_length'] = 4
         return models.fields.Field.formfield(self, **kwargs)
