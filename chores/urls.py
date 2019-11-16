bc9982a55e8a48e40a1cf162721a5de4fdf12a7d
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-11-16 13:30:42 +0100

    Whitespace sync, no real changes

--- chores/urls.py
+++ chores/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='chores'),
-    path('signup/<int:chore_id>/<int:ts>', views.signup, name='signup_chore'),
-    path('remove/<int:chore_id>/<int:ts>', views.remove_signup, name='remove_signup_chore'),
+      path('', views.index, name='chores'),
+      path('signup/<int:chore_id>/<int:ts>', views.signup, name='signup_chore'),
+      path('remove/<int:chore_id>/<int:ts>', views.remove_signup, name='remove_signup_chore'),
 ]
-
