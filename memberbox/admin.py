a55f7a39b5dd8cc68a4ae6fd1e53178c5b659aa6
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-08-17 21:14:07 +0200

    Simplify the list of memberboxes to make it more logical

--- memberbox/admin.py
+++ memberbox/admin.py
@@ -7,6 +7,7 @@ from .models import Memberbox
 
 class MemberboxAdmin(ImportExportModelAdmin, SimpleHistoryAdmin):
     search_fields = [ 'location', 'owner__first_name', 'owner__last_name', 'owner__email' ]
+    list_display = ('location', 'owner',)
     pass
 
 admin.site.register(Memberbox, MemberboxAdmin)
