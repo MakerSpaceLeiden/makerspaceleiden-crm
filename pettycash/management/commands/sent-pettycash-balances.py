c2cdb999d2d230528206cd3d663f7518d1de981d
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-10-18 17:30:45 +0200

    Feature: : Om te voorkomen dat iedereen gespamt wordt, ook als je geen SpaceTegoed gebruikt, krijg je deze mail alleen als er de afgelopen drie maanden überhaubt transacties geweest zijn of wanneer je saldo negatief is.

--- pettycash/management/commands/sent-pettycash-balances.py
+++ pettycash/management/commands/sent-pettycash-balances.py
@@ -46,6 +46,12 @@ class Command(BaseCommand):
         )
 
         parser.add_argument(
+            '--days',
+            dest='days', action='store', default = 100, type=int,
+            help='Ignore people who did not do a transaction in this many days (default is 100)'
+        )
+
+        parser.add_argument(
             '--save',
             dest='save', type = str, 
             help='Save the message as rfc822 blobs rather than sending. Useful as we sort out dkim on the server. Pass the output directory as an argument',
@@ -53,15 +59,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         verbosity = int(options['verbosity'])
+        cutoff_date = datetime.now(tz=timezone.utc)-timedelta(days=options['days'])
         rc = 0
-
+ 
         if options['save']:
             settings.EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
             settings.EMAIL_FILE_PATH = options['save']
 
         balances = PettycashBalanceCache.objects.order_by('balance')
         if  not options['all']:
-            balances = balances.filter(Q(balance__gt = Money(0,EUR)) | Q(balance__lt  = Money(0,EUR)))
+            balances = balances.filter(Q(balance__gt = Money(0,EUR)) | Q(balance__lt  = Money(0,EUR))).filter(Q(last__date__gt = cutoff_date))
 
         dest = settings.MAILINGLIST
         if options['to']:
@@ -69,7 +76,10 @@ class Command(BaseCommand):
 
         if options['direct']:
            dest = (balances.values_list('owner__email', flat=True))
-        
-        sendEmail(balances, dest)
+       
+        if balances.count(): 
+           sendEmail(balances, dest)
+        else:
+           print("No balances sent - none done since %s" % cutoff_date)
 
         sys.exit(rc)
