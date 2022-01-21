9a71fd339086f49a1102a03f60e33099d35938ce
Dirk-Willem van Gulik <dirkx@webweaving.org>
2022-01-22 00:58:53 +0100

    Add some extra heuristics/options

--- spaceapi/views.py
+++ spaceapi/views.py
@@ -28,10 +28,18 @@ def index(request):
         )
 
     state = aggregator_adapter.fetch_state_space()
-    context = {"open": 'false'}
+    context = {"open": "false"}
 
+    if state["lights_on"]:
+        context = {"open": "true"}  # must be lowercase
+
+    for e in state["machines"]:
+        m = e["machine"]
+        if "light" in m["name"].lower() and "state" in m and "on" in m["state"].lower():
+            context = {"open": "true"}  # must be lowercase
+
+    # {'machine': {'location_name': 'Main room', 'machine_id': 50, 'name': 'Main room lights'}, 'state': 'lights_off'},
     if bool(state["users_in_space"]):
-        context = {"open": 'true'} # must be lowercase
         lst = datetime.datetime(1970, 1, 1)
         for user in state["users_in_space"]:
             ulst = parser.parse(user["ts_checkin"])
