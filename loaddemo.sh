868e0071336db1da965c593f75aaaf532a80815b
Stephen Herbein <stephen@herbein.net>
2023-01-10 20:36:54 -0500

    fix django import errors due to versioning

--- loaddemo.sh
+++ loaddemo.sh
@@ -12,7 +12,7 @@ test -f db.sqlite3 && rm -f db.sqlite3
 
 python${VERSION} manage.py makemigrations
 python${VERSION} manage.py migrate
- export LC_ALL=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 echo
 echo Importing data
@@ -22,11 +22,11 @@ if test -f demo/example.json; then
 	python${VERSION} manage.py loaddata demo/example.json
 else
 	python${VERSION} manage.py import-wifi demo/wifi.csv
-	python${VERSION} manage.py import-machines demo/mac.csv 
-	python${VERSION} manage.py import-consolidated demo/consolidated.txt 
+	python${VERSION} manage.py import-machines demo/mac.csv
+	python${VERSION} manage.py import-consolidated demo/consolidated.txt
 	python${VERSION} manage.py pettycash-recache
-        python${VERSION} manage.py pettycash-activate-all-users
-        echo 
+    python${VERSION} manage.py pettycash-activate-all-users
+    echo
 	echo No invites with password-set requests sent. Passwords are all hardcoded to 1234 for:
 	grep @  demo/consolidated.txt
 fi
