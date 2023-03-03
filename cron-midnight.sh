c51bb9b4b2958d6cb1aef5b7e81518a55c8d8c6e
Dirk-Willem van Gulik <dirkx@webweaving.org>
2023-03-03 17:14:08 +0100

    Merge /etc/crontab.* file into main one

--- cron-midnight.sh
+++ cron-midnight.sh
@@ -8,3 +8,5 @@ cd $DIR || exit 1
 python manage.py pettycash-recache
 python manage.py clean_duplicate_history --auto > /dev/null
 python manage.py clean_old_history --days 1000 --auto > /dev/null
+python manage.py sent-ufo-reminders
+
