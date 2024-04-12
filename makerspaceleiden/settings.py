7aabe7475cd8fc290dad1d03a425a81306b4a890
Mark van der Klaauw <114411358+markvdk@users.noreply.github.com>
2024-04-12 14:23:21 +0200

    Adding navigation navbar and navpage

--- makerspaceleiden/settings.py
+++ makerspaceleiden/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "ultimaker.apps.UltimakerConfig",
     "spaceapi.apps.SpaceapiConfig",
     "memberlist",
+    "navigation",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
