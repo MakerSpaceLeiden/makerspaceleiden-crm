47659108c70a12d2901ea1c49ca73a1918174741
Dirk-Willem van Gulik <dirkx@webweaving.org>
2024-04-15 12:09:48 +0200

    Simple script for normal/small-change rollouts

---
+++ rollout-prod.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -x
+UK=$$
+
+trap cleanup INT
+cleanup () {
+	echo Cleaning up
+	rm -f /tmp/backup.$$ /tmp/backup.$UK /tmp/backup-static.$UK
+}
+
+(
+set -e
+. ./venv/bin/activate
+
+pip install -r requirements.txt  --quiet
+
+python manage.py dumpdata | gzip -c > /tmp/backup.$UK
+test -d static && tar zcf /tmp/backup-static.$UK static
+
+python manage.py collectstatic --no-input
+
+python manage.py makemigrations
+python manage.py migrate
+
+sudo apachectl restart
+)
+E=$?
+
+cleanup
+exit $E
+
+
+
