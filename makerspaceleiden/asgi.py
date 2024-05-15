acedbed74077aa6da6e4645f8864e5bf169adbc7
Younes Moustaghfir <younes@syntho.ai>
2024-05-15 20:48:49 +0200

    chore: adjust asgi file

--- makerspaceleiden/asgi.py
+++ makerspaceleiden/asgi.py
@@ -5,15 +5,27 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from selfservice.aggregator_adapter import initialize_aggregator_adapter
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "makerspaceleiden.settings")
 
 _application = get_asgi_application()
 
 from nodered.routing import websocket_urlpatterns  # noqa: E402
 
+
+def initialized_application():
+    initialize_aggregator_adapter(
+        os.environ.get("AGGREGATOR_BASE_URL", "http://127.0.0.1:5000"),
+        os.environ.get("AGGREGATOR_USERNAME", "user"),
+        os.environ.get("AGGREGATOR_PASSWORD", "pass"),
+    )
+    return _application
+
+
 application = ProtocolTypeRouter(
     {
-        "http": _application,
+        "http": initialized_application(),
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
