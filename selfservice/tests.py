6d25e17ae2b55cdaeb1972f772bf781b1ef20255
Luke Watts <luke@thisis.la>
2025-04-10 12:31:53 +0200

    refactor: refinements to improve readability
    - Move common test data to setup method
    - Adds explicit test case for "normal" user with entitlements
    - Switch to list comprehension for building machines list

--- selfservice/tests.py
+++ selfservice/tests.py
@@ -2,30 +2,40 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from acl.models import Machine, PermitType
+from acl.models import Entitlement, Machine, PermitType
 from members.models import User
 
 
 class AclTest(TestCase):
     def setUp(self):
-        User.objects.create_user(email="test@test.nl", password="testpassword")
         self.client = Client()
 
-    def test_record_instructions_no_permissions(self):
+        # Create common test data
+        self.admin = User.objects.create_superuser(
+            email="admin@test.nl", password="testpassword", is_superuser=True
+        )
         woodpermit, wasCreated = PermitType.objects.get_or_create(
             name="Wood Instruction", description="Can issue wood machine entitlement"
         )
         self.assertTrue(wasCreated)
+        self.woodpermit = woodpermit
 
-        Machine.objects.create(
+        self.machine = Machine.objects.create(
             name="First machine",
             node_name="Node1",
             description="Test",
             requires_instruction=True,
-            requires_permit=woodpermit,
+            requires_permit=self.woodpermit,
         )
 
-        success = self.client.login(email="test@test.nl", password="testpassword")
+    # Verify that a user with no permisssions can not
+    # add instructions for a machine they are not entitled for.
+    def test_record_instructions_no_permissions(self):
+        testUser = User.objects.create_user(
+            email="test@test.nl", password="testpassword"
+        )
+
+        success = self.client.login(email=testUser.email, password="testpassword")
         self.assertTrue(success)
 
         response = self.client.get(reverse("add_instruction"))
@@ -36,35 +46,44 @@ class AclTest(TestCase):
             response.content.decode("utf-8"),
         )
 
+    # Verify that a user with necessary permissions
+    # can add instructions for a machine they are entitled for.
     def test_record_instructions_with_permissions(self):
-        woodpermit, wasCreated = PermitType.objects.get_or_create(
-            name="Wood Instruction", description="Can issue wood machine entitlement"
+        testUser = User.objects.create_user(
+            email="test@test.nl", password="testpassword"
         )
-        self.assertTrue(wasCreated)
+        testUser.save()
 
-        Machine.objects.create(
-            name="First machine",
-            node_name="Node1",
-            description="Test",
-            requires_instruction=True,
-            requires_permit=woodpermit,
+        Entitlement.objects.create(
+            active=True, permit=self.woodpermit, holder=testUser, issuer=self.admin
         )
 
-        User.objects.create_superuser(
-            email="admin@test.nl", password="testpassword", is_superuser=True
-        )
-
-        success = self.client.login(
-            email="admin@test.nl",
-            password="testpassword",
-        )
+        success = self.client.login(email=testUser.email, password="testpassword")
         self.assertTrue(success)
 
         response = self.client.get(reverse("add_instruction"))
         self.assertEqual(response.status_code, 200)
 
         self.assertIn(
-            "First machine",
+            self.machine.name,
+            response.content.decode("utf-8"),
+        )
+        self.assertNotIn(
+            "Sorry - you have not had instructions on any machine yourself",
+            response.content.decode("utf-8"),
+        )
+
+    # Verify that a super user can add instructions
+    # for a machine they are entitled for.
+    def test_record_instructions_admin_user(self):
+        success = self.client.login(email=self.admin.email, password="testpassword")
+        self.assertTrue(success)
+
+        response = self.client.get(reverse("add_instruction"))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn(
+            self.machine.name,
             response.content.decode("utf-8"),
         )
         self.assertNotIn(
