d37c14af33b581cb8dee62ecf069672739870699
Luke Watts <luke@thisis.la>
2025-05-16 16:43:49 +0200

    refactor: use AppConfig for DI to simplify testing

--- spaceapi/tests.py
+++ spaceapi/tests.py
@@ -1,1 +1,22 @@
-# Create your tests here.
+from unittest import TestCase
+
+from django.apps import apps
+from django.test import Client
+
+from selfservice.tests.mocks import MockAggregatorAdapter
+
+
+class SpaceApiTest(TestCase):
+    apps.get_app_config("selfservice").aggregator_adapter = MockAggregatorAdapter()
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_redirect(self):
+        response = self.client.get("/spaceapi/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/spaceapi/0.13")
+
+    def test_basic(self):
+        response = self.client.get("/spaceapi/0.13")
+        self.assertEqual(response.status_code, 200)
