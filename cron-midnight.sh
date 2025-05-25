8611519a5b71475ad6cf58d7675b6f038f6316ea
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-05-25 18:14:47 +0200

    Production specific tweak to hunt down error in leavers/donations

--- cron-midnight.sh
+++ cron-midnight.sh
@@ -17,7 +17,14 @@ else
     . ./venv/bin/activate
 fi
 
+{
+	date
+	${POETRY_RUN}python manage.py pettycash-balance-check
+	${POETRY_RUN}python manage.py pettycash-transactions --days 1
+} >> /tmp/balance-debugging-dirkx.txt
+
 ${POETRY_RUN}python manage.py pettycash-recache
 ${POETRY_RUN}python manage.py clean_duplicate_history --auto > /dev/null
 ${POETRY_RUN}python manage.py clean_old_history --days 1000 --auto > /dev/null
 ${POETRY_RUN}python manage.py send-acl-reminders > /dev/null
+
