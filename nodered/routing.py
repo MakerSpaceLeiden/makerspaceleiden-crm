8aa296e59451d54d651275f8bb71600aab8afcc4
Luke Watts <luke@thisis.la>
2025-06-16 15:05:02 +0200

    fix: extend path matching

--- nodered/routing.py
+++ nodered/routing.py
@@ -4,5 +4,12 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"/comms", consumers.NodeREDWebsocketProxyConsumer.as_asgi()),
-    re_path(r"/dashboard/dashboard/socket\.io/", consumers.NodeREDDashboardSocketIOConsumer.as_asgi()),
+    re_path(
+        r"/dashboard/dashboard/socket\.io/",
+        consumers.NodeREDDashboardSocketIOConsumer.as_asgi(),
+    ),
+    re_path(
+        r"/dashboard/dashboard/socket\.io/(?P<path>.*)",
+        consumers.NodeREDDashboardSocketIOConsumer.as_asgi(),
+    ),
 ]
