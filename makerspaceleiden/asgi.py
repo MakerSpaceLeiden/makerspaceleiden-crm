abfeb5098e537cb6ce3c357de2ffcf6770bc0650
Luke Watts <luke@thisis.la>
2025-07-02 21:31:05 +0200

    chore: assign adapter

--- makerspaceleiden/asgi.py
+++ makerspaceleiden/asgi.py
@@ -3,6 +3,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.apps import apps
 from django.core.asgi import get_asgi_application
 
 from selfservice.aggregator_adapter import initialize_aggregator_adapter
@@ -15,11 +16,14 @@ from nodered.routing import websocket_urlpatterns  # noqa: E402
 
 
 def initialized_application():
-    initialize_aggregator_adapter(
+    adapter = initialize_aggregator_adapter(
         os.environ.get("AGGREGATOR_BASE_URL", "http://127.0.0.1:5000"),
         os.environ.get("AGGREGATOR_USERNAME", "user"),
         os.environ.get("AGGREGATOR_PASSWORD", "pass"),
     )
+
+    apps.get_app_config("selfservice").aggregator_adapter = adapter
+
     return _application
 
 
