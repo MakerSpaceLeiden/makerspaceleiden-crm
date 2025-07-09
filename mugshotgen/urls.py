721661ac1eaca611a70d327cc8bf1ff57c17a89c
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-07-09 22:46:10 +0200

    Reminder to fix this

--- mugshotgen/urls.py
+++ mugshotgen/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+# Caveat - not yet behind a @login/bearer protection
 urlpatterns = [
     path("<int:pk>", views.index, name="generate-fake-mugshot"),
 ]
