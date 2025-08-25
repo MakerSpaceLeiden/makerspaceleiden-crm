17decfa74ea350598fc0bd23036ecbf9f5303ba9
Luke Watts <luke@thisis.la>
2025-08-25 10:31:36 +0200

    chore: apply formatting

--- makerspaceleiden/oidc_provider_settings.py
+++ makerspaceleiden/oidc_provider_settings.py
@@ -1,5 +1,4 @@
 def userinfo(claims, user):
-    claims['name'] = '{0} {1}'.format(user.first_name, user.last_name)
-    claims['email'] = user.email
+    claims["name"] = "{0} {1}".format(user.first_name, user.last_name)
+    claims["email"] = user.email
     return claims
-
