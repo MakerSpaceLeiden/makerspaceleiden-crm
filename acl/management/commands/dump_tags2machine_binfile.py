75be67200ca7db55426681d6ff4c541ee8961780
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-10-09 14:26:08 +0200

    Fix bug in has; output now identical to C

--- acl/management/commands/dump_tags2machine_binfile.py
+++ acl/management/commands/dump_tags2machine_binfile.py
@@ -83,7 +83,7 @@ class Command(BaseCommand):
              if (idx > len(members)):
                  raise Exception(f"Tag entry {i} has a OOB reference to members data")
 
-             has =  int.from_bytes(members[idx:1],'big')
+             has =  int.from_bytes(members[idx:idx+1],'big')
              print(f"	Has: 		{has:02x}")
 
              needs =  int.from_bytes(members[idx+1:idx+2],'big')
