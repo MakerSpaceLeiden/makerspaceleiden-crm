05b20edddf9c1cc393af610fe7072089b308aec2
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-10-12 20:56:45 +0200

    Add a page to make it easier to track transactions

--- sumup_connector/urls.py
+++ sumup_connector/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # End user interaction
+    path("", views.index, name="sumup-index"),
+    # APIs for the payment terminals
     path("api/v1/sumup-pay", views.api1_sumup_pay, name="sumup-v1-pay"),
+    # External callback - called by SumUP from the internet.
     path(
         "api/v1/sumup-callback/<int:sumup_pk>-<int:timeint>-<str:hash>",
         views.api1_sumup_callback,
