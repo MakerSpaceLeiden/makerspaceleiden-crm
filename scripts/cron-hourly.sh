90f469279f9f49b57b8e0398b5a11c1850533534
Luke Watts <luke@thisis.la>
2025-10-19 17:34:12 +0200

    chore(cron): introduce hourly script

---
+++ scripts/cron-hourly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+test -f .env && source .env
+
+DIR=${DIR:-/usr/local/makerspaceleiden-crm}
+cd $DIR || exit 1
+
+uv venv --allow-existing --quiet
+source .venv/bin/activate
+
+DJANGO_RUN="uv run python manage.py"
+
+if !  ${DJANGO_RUN} version > /dev/null; then
+	echo Check with manage.py first - some compile errors.
+	exit 1
+fi
+
+${DJANGO_RUN} member_automated_checkout $*
