374db24ea6e4e35c91f08ccc5f649c6340b03655
Luke Watts <luke@thisis.la>
2025-10-19 14:59:57 +0000

    feat(members): adds handling for stale user sessions (#296)
    * feat(members): handle stale user sessions
    
    * feat(members): send email when checking out user
    
    * refactor: switch to use EmailMessage class
    
    * refactor: introduce error handling
    
    * fix: include URL base

---
+++ members/management/commands/member_automated_checkout.py
@@ -0,0 +1,54 @@
+from datetime import timedelta
+
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils import timezone
+
+from members.models import User
+
+MAX_HOURS_BEFORE_STALE = 8
+
+MEMBER_AUTOMATED_CHECKOUT_EMAIL_SUBJECT = "Your account has been checked out"
+
+
+class Command(BaseCommand):
+    help = "Automated checkout of members"
+
+    def handle(self, *args, **options):
+        """Checkouts members who have been checked in for more than configured maximum hours"""
+        checked_in = User.objects.filter(
+            is_onsite=True,
+            onsite_updated_at__lte=timezone.now()
+            - timedelta(hours=MAX_HOURS_BEFORE_STALE),
+        )
+
+        if len(checked_in) == 0:
+            self.stdout.write("No users to check out")
+            return
+
+        for u in checked_in:
+            try:
+                with transaction.atomic():
+                    u.checkout()
+                    EmailMessage(
+                        subject=MEMBER_AUTOMATED_CHECKOUT_EMAIL_SUBJECT,
+                        body=render_to_string(
+                            "members/email_checkout.txt",
+                            {
+                                "user": u,
+                                "url_space_state": settings.BASE
+                                + reverse("space_state"),
+                                "url_notification_settings": settings.BASE
+                                + reverse("notification_settings"),
+                            },
+                        ),
+                        from_email=settings.DEFAULT_FROM_EMAIL,
+                        to=[u.email],
+                    ).send()
+                    self.stdout.write(f"Checked out {u.email}")
+            except Exception as e:
+                self.stderr.write(f"Failed for {u.email}: {e}")
---
+++ members/tests/test_commands.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timezone
+
+import time_machine
+from django.core import mail
+from django.core.management import call_command
+from django.test import TestCase
+
+from chores.tests.factories import UserFactory
+from members.management.commands.member_automated_checkout import (
+    MEMBER_AUTOMATED_CHECKOUT_EMAIL_SUBJECT,
+)
+
+
+class MembersAutomatedCheckoutTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory(
+            email="member.author@example.com",
+            is_onsite=True,
+            onsite_updated_at=datetime(2025, 7, 14, 20, 00, 0, 0, tzinfo=timezone.utc),
+        )
+
+        self.stale_user = UserFactory(
+            email="stale.user@example.com",
+            is_onsite=True,
+            onsite_updated_at=datetime(2025, 7, 14, 13, 00, 0, 0, tzinfo=timezone.utc),
+        )
+
+    @time_machine.travel("2025-07-15 00:00")
+    def test_member_automated_checkout(self):
+        exactly_8h_user = UserFactory(
+            email="exactly.8h@example.com",
+            is_onsite=True,
+            onsite_updated_at=datetime(2025, 7, 14, 16, 0, 0, 0, tzinfo=timezone.utc),
+        )
+
+        call_command("member_automated_checkout")
+
+        exactly_8h_user.refresh_from_db()
+        self.user.refresh_from_db()
+        self.stale_user.refresh_from_db()
+
+        self.assertEqual(self.user.is_onsite, True)
+        self.assertEqual(exactly_8h_user.is_onsite, True)
+        self.assertEqual(self.stale_user.is_onsite, False)
+
+        self.assertIn(self.stale_user.email, [m.to[0] for m in mail.outbox])
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(
+            mail.outbox[0].subject, MEMBER_AUTOMATED_CHECKOUT_EMAIL_SUBJECT
+        )
